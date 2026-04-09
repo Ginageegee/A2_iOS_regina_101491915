f1dc1ab80ea8120113bcae0a85dae8107282983d
ginaslonimsky <regina.slonimsky@hotmail.com>
2026-04-08 22:00:43 -0400

    Add product functionality

--- A2_iOS_regina_101491915/AddProductView.swift
+++ A2_iOS_regina_101491915/AddProductView.swift
@@ -1,1 +1,43 @@
+//
+//  AddProductView.swift
+//  A2_iOS_regina_101491915
+//
+//  Created by Gina Slonimsky  on 2026-04-08.
+//
 
+import SwiftUI
+import CoreData
+
+struct AddProductView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.dismiss) private var dismiss
+
+    @State private var productID = ""
+    @State private var productName = ""
+    @State private var productDescription = ""
+    @State private var productPrice = ""
+    @State private var productProvider = ""
+
+    var body: some View {
+        Form {
+            Section(header: Text("Product Info")) {
+                TextField("Product ID", text: $productID)
+                    .keyboardType(.numberPad)
+
+                TextField("Product Name", text: $productName)
+                TextField("Description", text: $productDescription)
+
+                TextField("Price", text: $productPrice)
+                    .keyboardType(.decimalPad)
+
+                TextField("Provider", text: $productProvider)
+            }
+
+            Button("Save Product") {
+                saveProduct()
+            }
+        }
+        .navigationTitle("Add Product")
+    }
+
+}
