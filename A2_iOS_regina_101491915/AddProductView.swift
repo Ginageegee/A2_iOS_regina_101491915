6aeb6225865fe22afb447b798af4256dd68e698e
ginaslonimsky <regina.slonimsky@hotmail.com>
2026-04-08 21:44:33 -0400

    Initial commit

---
+++ A2_iOS_regina_101491915/AddProductView.swift
@@ -0,0 +1,18 @@
+//
+//  AddProductView.swift
+//  A2_iOS_regina_101491915
+//
+//  Created by Gina Slonimsky  on 2026-04-08.
+//
+
+import SwiftUI
+
+struct AddProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddProductView()
+}
---
+++ A2_iOS_regina_101491915/ProductListView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductListView.swift
+//  A2_iOS_regina_101491915
+//
+//  Created by Gina Slonimsky  on 2026-04-08.
+//
+
+import SwiftUI
+
+struct ProductListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductListView()
+}
