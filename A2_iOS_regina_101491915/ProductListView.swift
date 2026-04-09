6944d5f76d9039d72d3f972c98931824312e776f
ginaslonimsky <regina.slonimsky@hotmail.com>
2026-04-08 21:58:47 -0400

    Product list functionality

--- A2_iOS_regina_101491915/ProductListView.swift
+++ A2_iOS_regina_101491915/ProductListView.swift
@@ -5,3 +5,29 @@
 //  Created by Gina Slonimsky  on 2026-04-08.
 //
 
+import SwiftUI
+import CoreData
+
+struct ProductListView: View {
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Product.productID, ascending: true)],
+        animation: .default)
+    private var products: FetchedResults<Product>
+
+    var body: some View {
+        List {
+            ForEach(products) { product in
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(product.productName ?? "No Name")
+                        .font(.headline)
+
+                    Text(product.productDescription ?? "No Description")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .padding(.vertical, 4)
+            }
+        }
+        .navigationTitle("Product List")
+    }
+}
